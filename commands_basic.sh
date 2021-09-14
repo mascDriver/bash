901aaad6febb205c9a4b139cf91cea4ca9aabb7c
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-14 13:07:42 -0300

    Create commands_basic.sh

---
+++ commands_basic.sh
@@ -0,0 +1,13 @@
+pwd #dir atual
+whoami # user atual
+cat file.txt # ler aquivo
+man command # help do comando
+echo "REM" > musicas-favoritas.txt # colocar no arquivo
+echo "Faithless" >> musicas-favoritas.txt # adicionar sem sobrescrever
+rmdir dir # remove diretornio empty
+rm -r workspace # remover tudo
+cp bemvindo2.txt bemvindo.txt # copiando arquivo
+cp -r projetos-java projetos-c# # copiando diretorio
+zip work.zip workspace/ # zipando diretorio
+zip -r work.zip workspace/ # zipado todo conteudo do diretorio
+unzip -q work.zip # dezipar
