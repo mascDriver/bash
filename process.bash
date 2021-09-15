91aa2f09c66816bccea983ef4f7efcb07d2264a5
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-15 09:32:34 -0300

    Create process.bash

---
+++ process.bash
@@ -0,0 +1,6 @@
+ps #ver processos no terminal
+ps -e #ver todos os processos
+kill pid #matar processo
+kill -9 pid #mata processo imediatamente
+ps -ef #informacoes a mais
+ps -ef | grep firefox #filtro de processos
