7f4ea6959057d276c5d07aadbfcbcaaeb6f9ee17
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-16 10:37:29 -0300

    Create permission.bash

---
+++ permission.bash
@@ -0,0 +1,6 @@
+r # read
+w # write
+x # exec
+chmod +x file.txt # atribuir permissao de execução de um arquivo
+chmod -x file.txt # remover permissao de execução de um arquivo
+ls -l file.txt #ver permissoes de um arquivo
