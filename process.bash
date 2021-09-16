c91598b12f2f6e4b3da38e7f67768691586a753b
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-16 10:20:10 -0300

    Update process.bash

--- process.bash
+++ process.bash
@@ -4,3 +4,12 @@ kill pid #matar processo
 kill -9 pid #mata processo imediatamente
 ps -ef #informacoes a mais
 ps -ef | grep firefox #filtro de processos
+pstree #arvore de processo
+
+# transferir pro back um processo
+firefox #executar processo
+ctrl + z #parar processo
+jobs #ver processos e ver indice
+bg 1 #mandar para bg indice
+jobs #ver processo
+fg1 #mandar para front
