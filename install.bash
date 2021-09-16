380db46432bc2ac3270761263354f1d4eb798975
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-16 11:28:39 -0300

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,7 @@
+sudo dpkg -i google-chrome-stable_current_amd64.deb # instalar arquivos .deb
+sudo dpkg -r google-chrome-stable # remover pacote 
+
+#install tar.gz
+./configure
+make
+sudo make install
